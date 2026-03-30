55e5670f85f2f20189b240519630653ee48b45c2
Luciano Villanueva <lucianoivillanuevar@gmail.com>
2026-03-30 20:55:07 -0300

    Base de la pagina con tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://lucianoVillanuevaR.github.io',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
 
