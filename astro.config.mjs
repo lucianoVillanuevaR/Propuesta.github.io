bbba705fc21e8fad715cbcbddf8bb850fadaa3e6
Luciano Villanueva Ramirez <lucianoivillanuevar@gmail.com>
2026-03-30 21:03:38 -0300

    Update base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://lucianoVillanuevaR.github.io',
+  base: '/Propuesta.github.io/', 
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
